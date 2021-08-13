951c338ef2aa1135ca962fdf52232142716e4cc6
HenryNguyenHCMVN <huyhumor@gmail.com>
2021-08-13 11:05:41 +0700

    13082021

--- src/app/core/share/data/data.service.ts
+++ src/app/core/share/data/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs';
 
 
@@ -21,11 +21,14 @@ export class DataService {
   form: FormGroup = new FormGroup({
     taiKhoan: new FormControl('',[Validators.required, Validators.minLength(3)]),
     matKhau: new FormControl('',[Validators.required, Validators.pattern("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}")]),
+    conFirmMatKhau: new FormControl(null),
     hoTen: new FormControl('', [Validators.required, Validators.minLength(3)]),
     maNhom: new FormControl(''),
     email: new FormControl('',[Validators.required, Validators.pattern("[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}")]),
     soDT: new FormControl('',[Validators.required, Validators.pattern("^[0-9]*$")]),
-    maLoaiNguoiDung: new FormControl('Student'),
+    maLoaiNguoiDung: new FormControl('Student')
+  },{
+    validators: [ValidationMatchPass.match('matKhau','conFirmMatKhau')]
   })
 
   formEdit: FormGroup = new FormGroup({
@@ -63,4 +66,26 @@ export class DataService {
     maDanhMucKhoaHoc: new FormControl(''),
     taiKhoanNguoiTao: new FormControl(''),
   })
+
+
+}
+
+export default class ValidationMatchPass {
+  static match(passWord: string, checkpassWord: string): ValidatorFn {
+      return (controls: AbstractControl) => {
+          const pass = controls.get(passWord);
+          const checkPass = controls.get(checkpassWord);
+
+          if (checkPass?.errors && !checkPass.errors.matching) {
+              return null;
+          }
+
+          if (pass?.value !== checkPass?.value) {
+              controls.get(checkpassWord)?.setErrors({ matching: true });
+              return { matching: true };
+          } else {
+              return null;
+          }
+      };
+  }
 }
